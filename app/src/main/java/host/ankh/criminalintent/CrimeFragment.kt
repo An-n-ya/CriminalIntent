bd03d9159fd4dcfdc5be4c45f85db541016ab005
ankh <ankh04@icloud.com>
2023-12-12 22:49:13 +0800

    dialog complete

--- app/src/main/java/host/ankh/criminalintent/CrimeFragment.kt
+++ app/src/main/java/host/ankh/criminalintent/CrimeFragment.kt
@@ -16,7 +16,9 @@ import java.util.*
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
-class CrimeFragment : Fragment() {
+private const val DIALOG_DATE = "dialog_date"
+private const val REQUEST_DATE = 0
+class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
@@ -44,10 +46,10 @@ class CrimeFragment : Fragment() {
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
+//        dateButton.apply {
+//            text = crime.date.toString()
+//            isEnabled = false
+//        }
         return view
     }
 
@@ -86,11 +88,23 @@ class CrimeFragment : Fragment() {
                 crime.isSolved = isChecked
             }
         }
+
+        dateButton.setOnClickListener {
+            DatePickerFragment.newInstance(crime.date).apply {
+                setTargetFragment(this@CrimeFragment, REQUEST_DATE)
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+            }
+        }
     }
 
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDateSelected(date: Date) {
+        crime.date = date
+        updateUI()
     }
 
     private fun updateUI() {
@@ -112,4 +126,5 @@ class CrimeFragment : Fragment() {
             }
         }
     }
+
 }
